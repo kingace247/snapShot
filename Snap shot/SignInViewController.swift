29d63425f30ffb6e7855a3d0ecb317a60fe3778f
David Garza <davidagarza247@gmail.com>
2017-04-30 22:49:20 -0500

    First draft

---
+++ Snap shot/SignInViewController.swift
@@ -0,0 +1,51 @@
+//
+//  Sign In ViewController.swift
+//  Snap shot
+//
+//  Created by David Garza on 4/30/17.
+//  Copyright © 2017 David Garza. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import Firebase
+
+class SignInViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func SignUpTapped(_ sender: Any) {
+        
+        
+        FIRAuth.auth()?.signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
+            print("We tried to sign in ")
+            if error != nil {
+                print("Hey we have an error:\(error)")
+                
+                FIRAuth.auth()?.createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: { (user, error) in
+                    print("we tried to create User")
+                    
+                    if error != nil {
+                       print("We have an error:\(error)")
+                    }else{
+                        print("")
+                    }
+                })
+                
+            }else{
+                print("Signed in successfully")
+                
+            }
+        })
+        
+    }
+    
+    
+}
+
